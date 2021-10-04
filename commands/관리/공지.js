d425f968f1c9d21953805cef50534183c3b3ded0
ZREO_ZREO <zreozreo1234@gmail.com>
2021-10-04 09:32:19 +0900

    bit commit

--- commands/관리/공지.js
+++ commands/관리/공지.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js-selfbot');
 const client = new Discord.Client();
-const {prefix, basic, color, errorcolor, owner, ownerid, img, img2, img3, img4, covidapi, hpapikey} = require("../../config.json");
+const { prefix, basic, color, errorcolor, owner, ownerid, img, img2, img3, img4, covidapi, hpapikey } = require("../../config.json");
 
 module.exports = {
     name: '공지',
@@ -9,65 +9,53 @@ module.exports = {
 
         const announcement = message.content.slice(prefix.length + this.name.length + 1)
         const user = message.author
-        const embed = new Discord.MessageEmbed()
-        .setAuthor(`ANNOUNCE BY ${owner}`, img)
-        .setThumbnail(img)
-        .setTitle(`${message.guild.name} 의 공지사항`)
-        .setDescription(`**${announcement}**`)
-        .setFooter(`${owner}`, img)
-        .setTimestamp()
-        .setColor(`${color}`)
 
-        const Embed2 = new Discord.MessageEmbed() 
-        .setAuthor(`ERROR REPORTED by ${owner}`, img)
-        .setThumbnail(img)
-        .setTitle(`특정 유저에게 메시지를 보낼 수 없습니다`)
-        .setFooter(`${owner}`, img)
-        .setTimestamp()
-        .setColor(`${errorcolor}`)
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(`ANNOUNCE BY ${owner}`, img)
+            .setThumbnail(img)
+            .setTitle(`${message.guild.name} 의 공지사항`)
+            .setDescription(`**${announcement}**`)
+            .setFooter(`${owner}`, img)
+            .setTimestamp()
+            .setColor(`${color}`)
+
+        const Embed2 = new Discord.MessageEmbed()
+            .setAuthor(`ERROR REPORTED by ${owner}`, img)
+            .setThumbnail(img)
+            .setTitle(`${member.user.username}#${member.user.discriminator}에게 메시지를 보낼 수 없습니다`)
+            .setFooter(`${owner}`, img)
+            .setTimestamp()
+            .setColor(`${errorcolor}`)
 
         const Embed3 = new Discord.MessageEmbed()
-        .setAuthor(`ANNOUNCE BY ${owner}`, img)
-        .setThumbnail(img)
-        .setTitle(`DM으로 공지를 보냈어요!`)
-        .setFooter(`${owner}`, img)
-        .setTimestamp()
-        .setColor(`${color}`)
+            .setAuthor(`ANNOUNCE BY ${owner}`, img)
+            .setThumbnail(img)
+            .setTitle(`DM으로 공지를 보냈어요!`)
+            .setFooter(`${owner}`, img)
+            .setTimestamp()
+            .setColor(`${color}`)
 
-        const Embed4 = new Discord.MessageEmbed() 
-        .setAuthor(`ERROR REPORTED by ${owner}`, img)
-        .setThumbnail(img)
-        .setTitle(`${user.tag} 공지를 사용할 권한이 없습니다`)
-        .setFooter(`${owner}`, img)
-        .setTimestamp()
-        .setColor(`${errorcolor}`)
+        const Embed4 = new Discord.MessageEmbed()
+            .setAuthor(`ERROR REPORTED by ${owner}`, img)
+            .setThumbnail(img)
+            .setTitle(`${user.tag} 공지를 사용할 권한이 없습니다`)
+            .setFooter(`${owner}`, img)
+            .setTimestamp()
+            .setColor(`${errorcolor}`)
 
 
-        if (message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("ADMINISTRATOR") || message.member.id == ownerid)
-        {
-            if(message.member != null)
-            {
-                message.member.guild.members.cache.array().forEach(userlist => {
-                    if(userlist.user.bot) return;
-                    try
-                    {
-                        userlist.user.send(embed)
-                    }
-                    catch(e)
-                    {
-                        message.channel.send(Embed2)
-                        .then(msg => {setTimeout(() => msg.delete(), 3000)})
-                    }
-                    
-                });
-            }
-            message.channel.send(Embed3)
-            .then(msg => {setTimeout(() => msg.delete(), 3000)})
+        if (message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("ADMINISTRATOR") || message.member.id == ownerid) {
+            message.guild.members.cache.forEach(member => {
+                member.send(embed)
+                .then(console.log(`[+] DM Success  | ${member.user.username}#${member.user.discriminator}`))
+                .catch(e => console.error(`[-] DM Fail | ${member.user.username}#${member.user.discriminator}`));
+            })
+            message.channel.send(Embed3).then(message.delete({ timeout: 15000 }));
         }
-        else
-        {
+        else {
             message.channel.send(Embed4)
-            .then(msg => {setTimeout(() => msg.delete(), 3000)})
+                .then(msg => { setTimeout(() => msg.delete(), 3000) })
         }
 
     }
---
+++ commands/기타/퇴근.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js-selfbot');
+const client = new Discord.Client();
+const { prefix, basic, color, errorcolor, owner, ownerid, img, img2, img3, img4, covidapi, hpapikey } = require("../../config.json");
+
+module.exports = {
+    name: '퇴근',
+    execute(message) {
+        message.delete()
+        const user = message.author
+
+        const Embed = new Discord.MessageEmbed()
+            .setTitle(`출퇴근 기록`)
+            .setDescription(`**${user.username}#${user.discriminator} 님이 퇴근하셨습니다**`)
+            .setTimestamp()
+            .setColor(`${color}`)
+
+        if (!message.member.hasPermission("ADMINISTRATOR")) { return; }
+
+        client.channels.get("894058960414470224").send(Embed)
+
+
+    }
+}
